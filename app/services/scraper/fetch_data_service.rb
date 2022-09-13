5f1e1ec665a0433883ba19755a2e3009e57c0674
Bartłomiej Nowak <bartlomiejnowak@MacBook-Pro-Bartomiej.local>
2022-09-13 14:23:16 +0200

    Added whole data to database

--- app/services/scraper/fetch_data_service.rb
+++ app/services/scraper/fetch_data_service.rb
@@ -4,8 +4,8 @@ require 'httparty'
 
 module Scraper
   class FetchDataService
-    POPULARNE = "popularne" #14 stron ZROBIONE
-    LECZENIE = "leczenie" #704 stron ZROBIONE
+    POPULARNE = "popularne" #14 stron
+    LECZENIE = "leczenie" #704 stron
     POTRZEBY = "potrzeby" #970 stron
     ZWIERZETA = "zwierzeta" # 1755 stron
     PROJEKTY = "projekty" # 992 stron
@@ -14,10 +14,11 @@ module Scraper
     # Lacznie około zbiorek: 26610
 
     def runner
-
-
-
-
+      get_data_from_category(POPULARNE)
+      get_data_from_category(LECZENIE)
+      get_data_from_category(POTRZEBY)
+      get_data_from_category(ZWIERZETA)
+      get_data_from_category(PROJEKTY)
     end
 
     def data
@@ -32,7 +33,7 @@ module Scraper
     def get_data_from_category(category)
       page = 0
       basic_url = "https://pomagam.pl"
-      i = 1
+      #i = 1
 
       url = change_url(page, category)
       unparsed_page = HTTParty.get(url)
@@ -44,43 +45,38 @@ module Scraper
         parsed_page = Nokogiri::HTML(unparsed_page['html'])
 
         parsed_page.css(".project-card.extra-shadow").each do |collection_card|
-          # DLUZSZE POBIERANIE DANYCH
-          slug = collection_card.children[1].attributes["href"].value
-          slug_stats_url = basic_url + slug + "/stats"
-          request = HTTParty.get(slug_stats_url)
-          parsed_stats = to_json(request.body)
 
 
-          data << {
-            # DLUZSZE POBIERANIE DANYCH
+          data = {
             # number: i,
             category: category,
-            external_collection_id: collection_card.css("div.content-wrap").css("span")[0].attributes["id"].value[7...],
             title: collection_card.attributes["title"].value,
-            slug: slug,
-            amount: parsed_stats[:pledge],
-            number_of_donors: parsed_stats[:count],
-            percentage: parsed_stats[:percentage]
+            slug: collection_card.children[1].attributes["href"].value,
+            external_collection_id: collection_card.css("div.content-wrap").css("span")[0].attributes["id"].value[7...],
+            #amount:  collection_card.css("div.content-wrap").css("span").children.text
+            amount:  format_amount_to_int(collection_card.css("div.content-wrap").css("span").children.text)
           }
           # i = i + 1
 
 
-          data.each do |fetched_data_collection|
+          # data.each do |fetched_data_collection|
+
+            # collection = Collection.find_by(external_collection_id: fetched_data_collection[:external_collection_id])
+            # if collection
+            #   next if collection.updated_at.to_date == Time.current.to_date
+            #
+            #   # Collection.update(parse_data(fetched_data_collection).except(:external_collection_id))
+            #   Collection.update(collection_update_data(fetched_data_collection))
+            # else
+            #   # Collection.create(parse_data(fetched_data_collection))
+            #
+            #   Collection.create(collection_create_data(fetched_data_collection))
+            # end
+
             # binding.pry
-            collection = Collection.find_by(external_collection_id: fetched_data_collection[:external_collection_id])
-            if collection
-              next if collection.updated_at.to_date == Time.current.to_date
+            Collection.upsert(data, unique_by: :index_collections_on_external_collection_id)
 
-              # Collection.update(parse_data(fetched_data_collection).except(:external_collection_id))
-              Collection.update(collection_update_data(fetched_data_collection))
-            else
-              # Collection.create(parse_data(fetched_data_collection))
-
-              Collection.create(collection_create_data(fetched_data_collection))
-            end
-          end
-          # puts data
-          puts page
+          # end
         end
 
         page = page + 1
@@ -98,15 +94,19 @@ module Scraper
       }
     end
 
+    def format_amount_to_int(amount)
+      amount.to_s
+      amount.tr('^0-9', '')
+
+    end
+
     def collection_create_data(data)
       {
         category: data[:category],
         slug: data[:slug],
         amount: data[:amount],
-        donator: data[:amount],
-        percentage: data[:percentage],
         external_collection_id: data[:external_collection_id],
-        title: data[:title],
+        title: data[:title]
       }
     end
 
--- db/migrate/20220913085744_delete_collums.rb
+++ db/migrate/20220913085744_delete_collums.rb
@@ -2,6 +2,5 @@ class DeleteCollums < ActiveRecord::Migration[7.0]
   def change
     remove_column :collections, :donator
     remove_column :collections, :percentage
-
   end
 end
